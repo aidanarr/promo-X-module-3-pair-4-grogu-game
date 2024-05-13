12c4b31b403e30c7204cabb1f56f8878de8eee3e
luciadelafuente <luciadelaf1991@gmail.com>
2024-05-13 11:23:13 +0200

    dice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,17 @@ import "../styles/App.scss"
 import {useState} from 'react';
 import Header from "./Header";
 import Board from "./Board";
+import Dice from "./Dice";
 
 function App() {
   const [grogu, setGrogu] = useState(1);
   const [dice, setDice] = useState(null);
-  const [game, setGame] = useState('ongoing');
+  const [game, setGame] = useState('En curso.');
   const [cookies, setCookies] = useState(["🍪", "🍪", "🍪"]);
   const [eggs, setEggs] = useState(["🥚", "🥚", "🥚"]);
   const [frogs, setFrogs] = useState(["🐸", "🐸", "🐸"]);
+  const [message, setMessage] = useState("");
+  let hiddenClass = "hidden";
 
   function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -17,21 +20,32 @@ function App() {
 
   function rollDice() {
     const randomNumber = getRandomNumber(4);
-    console.log(randomNumber);
     setDice(randomNumber);
 
     if (randomNumber === 4) {
       setGrogu(grogu + 1);
+      setMessage("Grogu ha avanzado una casilla.");
     } else if (randomNumber === 1 && cookies.length > 0) {
-      setCookies(cookies.slice(1))
+      setCookies(cookies.slice(1));
+      setMessage("Se ha descargado una cookie.");
     } else if (randomNumber === 2 && eggs.length > 0) {
-      setEggs(eggs.slice(1))
+      setEggs(eggs.slice(1));
+      setMessage("Se ha descargado un huevo.");
     } else if (randomNumber === 3 && frogs.length > 0) {
-      setFrogs(frogs.slice(1))
+      setFrogs(frogs.slice(1));
+      setMessage("Se ha descargado una rana.");
     }
+    
     
   }
 
+  const gameStatus = () => {
+    if (grogu === 7){
+      setGame("El juego ha terminado.")
+    }else if (cookies.length === 0 && eggs.length === 0 && frogs.length === 0){
+      setGame("El juego ha terminado.")
+    }
+  }
 
 
   return (
@@ -40,11 +54,12 @@ function App() {
     <Header />
     <main className="page">
     <Board moveGrogu={grogu} />
-      <section>
-        <button className="dice" onClick={rollDice}>Lanzar Dado</button>
-        <div className="game-status">En curso</div>
+      <section className= "dice-box">
+        <Dice rollDice={rollDice} />
+        <p className={dice === null ? hiddenClass : ""}>Ha salido el número: {dice}</p>
+        <p>{message}</p>
+        <div className="game-status">{game}</div>
       </section>
-
       <section className="goods-container">
         <div className="goods-item">{cookies}</div>
       </section>
---
+++ src/components/Dice.jsx
@@ -0,0 +1,13 @@
+function Dice({rollDice}) {
+    const handleClick = (event) => {
+        rollDice();
+    
+    }
+  return (
+    <>
+          <button className="dice" onClick={handleClick}>🎲Lanzar dado🎲</button>
+    </>
+  )
+}
+
+export default Dice
